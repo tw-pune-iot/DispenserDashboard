e5eff86cae8996fe14e2fba44119e41fd6a70022
Supriya <supriyagole12@gmail.com>
2016-07-09 05:30:15 +0530

    Sumit/Supriya/Jai : Added basic water consumption functionality page

---
+++ app/statistics/statistics.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('myApp.statistics', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/statistics', {
+            templateUrl: 'statistics/statistics.html',
+            controller: 'statisticsCtrl'
+        });
+    }])
+
+    .controller('statisticsCtrl', function($scope) {
+        $scope.getWaterConsumption = function () {
+           return 4.75;
+        };
+
+        $scope.initialWaterConsumptionTime = function (){
+          return "12AM";
+        };
+
+        $scope.amountOfWaterLeftForConsumption = function () {
+           return "4.0";
+        };
+
+        $scope.getWaterBottleMap = function () {
+            var bottles = [];
+            var consumption = $scope.getWaterConsumption();
+            var numberOfTotalBottlesFinished = Math.floor(consumption);
+            var remainingWater = consumption - numberOfTotalBottlesFinished;
+            for(var index=0;index<numberOfTotalBottlesFinished;index++){
+                bottles.push("../resources/1litrebottle.png");
+            }
+            if(remainingWater>=0.25 && remainingWater<0.5){
+                bottles.push("../resources/quarterLitrebottle.png");
+            }
+            if(remainingWater>=0.5 && remainingWater<0.75){
+                bottles.push("../resources/halfLitrebottle.png");
+            }
+            if(remainingWater>=0.75){
+                bottles.push("../resources/threeQuarterLitrebottle.png");
+            }
+            console.log(bottles);
+            return bottles;
+        }
+    });
