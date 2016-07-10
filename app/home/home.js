a47aae6b823d7ff8ceb9c4b6b63eb5ba3857cd86
kodimalashirisha <kodimalashirisha@in-kodimalashirisha.local>
2016-07-10 18:20:05 +0530

    Showing water consumption with realtime data on front page - Supriya/Ranju

--- app/home/home.js
+++ app/home/home.js
@@ -9,10 +9,51 @@ angular.module('dispenser.home', ['ngRoute'])
         });
     }])
 
-    .controller('homeController', function($scope) {
+    .controller('homeController', function($scope,$http) {
+        $scope.info = false;
+        $scope.allData = null;
         $scope.topTenList = [{'id':'1', 'consumption':'2L'},{'id':'6','consumption':'5L'},
                                 {'id':'3','consumption':'2L'},{'id':'8','consumption':'4L'},
                                 {'id':'4','consumption':'3L'},{'id':'9','consumption':'1L'},
                                 {'id':'5','consumption':'4L'},{'id':'10','consumption':'1L'}];
 
+        $http.get('http://10.131.127.153:8083/api/users').then(function(data){
+            $scope.allData = data.data;
+        });
+
+        $scope.getData = function () {
+            $scope.consumptionAmount = 0;
+            $http.get('http://10.131.127.153:8083/api/waterdispenser/consumption/empId/'+$scope.empId).success(function(response){
+                response.forEach(function (eachEntry) {
+                    $scope.consumptionAmount += eachEntry.consumptionAmount;
+                });
+                $scope.needToDrink = 3700 - $scope.consumptionAmount;
+                $scope.resultMessage = $scope.getResultMessage($scope.needToDrink);
+            });
+        };
+
+        $scope.getEmpName = function(){
+            var matchUser = null;
+            $scope.allData.forEach(function (user) {
+                if(user.empId == $scope.empId){
+                    matchUser = user;
+                }
+            });
+            return matchUser.employeeName;
+        };
+        $scope.getResultMessage = function(water){
+            if(water){
+                return "You need to drink " + water + " ml water more!!!";
+            }
+            return "Great! You have covered daily limit of the day(3700 ml)";
+        }
+
+        $scope.showUserData = function () {
+            $scope.info= true;
+            $scope.getData();
+            $scope.empName = $scope.getEmpName();
+        };
+
     });
+
+
---
+++ app/resources/text.js
@@ -0,0 +1,11 @@
+[
+  {
+    "id": "1",
+    "consumption": "2L"
+  },
+  {
+    "id": "2",
+    "consumption": "2L"
+  }
+]
+
